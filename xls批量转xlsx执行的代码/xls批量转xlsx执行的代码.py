22ad24281e8ee57a9ab8ad441084203be8b12cd4
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2022-12-10 09:59:42 +0800

    Add files via upload

---
+++ xls批量转xlsx执行的代码/xls批量转xlsx执行的代码.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import os
+import win32com.client as win32
+
+# 输入目录
+inputdir = r'C:\Users\pdcfi\Desktop\test\收入确认表\sample'
+# 输出目录
+outputdir = r'C:\Users\pdcfi\Desktop\test\收入确认表\sample\res'
+if not os.path.exists(outputdir):
+    os.mkdir(outputdir)
+
+# 三个参数：父目录；所有文件夹名（不含路径）；所有文件名
+for parent, dirnames, filenames in os.walk(inputdir):
+    for fn in filenames:
+        if fn.split('.')[-1] == "xls":
+            filedir = os.path.join(parent, fn)
+            print(filedir)
+            excel = win32.gencache.EnsureDispatch('Excel.Application')
+            wb = excel.Workbooks.Open(filedir)
+            # xlsx: FileFormat=51
+            # xls:  FileFormat=56
+            wb.SaveAs((os.path.join(outputdir, fn.replace('xls', 'xlsx'))), FileFormat=51)
+            wb.Close()
+            excel.Application.Quit()
