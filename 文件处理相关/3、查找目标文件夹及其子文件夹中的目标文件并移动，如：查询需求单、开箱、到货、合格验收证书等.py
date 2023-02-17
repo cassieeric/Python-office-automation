6dfce4c4164f626a0cc51972ac9f8a6ae1213d9d
Python_web crawler <31296722+cassieeric@users.noreply.github.com>
2023-02-17 22:10:21 +0800

    Add files via upload

---
+++ 文件处理相关/3、查找目标文件夹及其子文件夹中的目标文件并移动，如：查询需求单、开箱、到货、合格验收证书等.py
@@ -0,0 +1,24 @@
+# 参考博客：https://blog.csdn.net/weixin_44476410/article/details/116063808
+import shutil
+import os
+
+
+def copy_file(path):
+    # （root，dirs，files）分别为：遍历的文件夹，遍历的文件夹下的所有文件夹，遍历的文件夹下的所有文件
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if "需求单" in file:
+                shutil.copyfile(root + '\\' + file, target_path + '\\' + file)
+                print(root + '\\' + file + ' 复制成功-> ' + target_path)
+        for dir_in in dirs:
+            copy_file(dir_in)
+
+
+if __name__ == '__main__':
+    # 文件夹路径
+    source_path = r'C:\Users\pdcfi\Desktop\test\需求单'
+    # 输出路径
+    target_path = r'C:\Users\pdcfi\Desktop\test\res'
+    copy_file(source_path)
+
+
